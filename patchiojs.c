e055d25f6c8b8d06c9eb0509ed2f080dbc4a0dd9
Stefan Scherer <scherer_stefan@icloud.com>
2015-08-30 18:26:41 +0200

    Replace strstr with memmem

--- patchiojs.c
+++ patchiojs.c
@@ -4,11 +4,42 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* Copyright (c) 2005 Pascal Gloor <pascal.gloor@spale.com> */
+void *
+mymemmem(const void *l, size_t l_len, const void *s, size_t s_len) {
+  char *cur, *last;
+  const char *cl = (const char *)l;
+  const char *cs = (const char *)s;
+
+  if (l_len == 0 || s_len == 0) {
+    return NULL;
+  }
+
+  if (l_len < s_len) {
+    return NULL;
+  }
+
+  if (s_len == 1) {
+    return memchr(l, (int)*cs, l_len);
+  }
+
+  last = (char *)cl + l_len - s_len;
+
+  for (cur = (char *)cl; cur <= last; cur++) {
+    if (cur[0] == cs[0] && memcmp(cur, cs, s_len) == 0) {
+      return cur;
+    }
+  }
+
+  return NULL;
+}
+
 int __cdecl main(int argc, char **argv)
 {
   FILE *f;
   long fsize;
   char *string;
+  char *pattern;
   char *match;
 
   // Validate the parameters
@@ -20,6 +51,7 @@ int __cdecl main(int argc, char **argv)
     return 1;
   }
 
+  printf("Reading %s\n", argv[1]);
   f = fopen(argv[1], "rb");
   fseek(f, 0, SEEK_END);
   fsize = ftell(f);
@@ -31,10 +63,15 @@ int __cdecl main(int argc, char **argv)
 
   string[fsize] = 0;
 
-  match = strstr(string, "dnsopts.hints = dns.ADDRCONFIG;");
+  printf("Searching for pattern\n");
+  pattern = "dnsopts.hints = dns.ADDRCONFIG;";
+  match = mymemmem(string, fsize, pattern, strlen(pattern));
   if (match) {
+    printf("Patching dnsopts.hints\n");
     strncpy(match, "dnsopts.hints = 0;//", 20);
   }
+
+  printf("Writing %s\n", argv[1]);
   f = fopen(argv[1], "wb");
   fwrite (string , sizeof(char), fsize, f);
   fclose (f);
