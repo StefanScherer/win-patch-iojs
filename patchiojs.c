3f0935b349503e53110d1d4d8f6f3aca0001f368
Stefan Scherer <scherer_stefan@icloud.com>
2015-08-30 17:42:25 +0200

    Fix compile errors on OSX

--- patchiojs.c
+++ patchiojs.c
@@ -2,37 +2,38 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int __cdecl main(int argc, char **argv)
 {
 
-    // Validate the parameters
-    if (argc != 2) {
-        printf("usage: %s <iojsexe>\n", argv[0]);
-        printf("patchiojs patches the iojs.exe/node.exe to run in a Windows container.\n");
-        printf("%s example usage\n", argv[0]);
-        printf("   %s \"C:\\Program Files\\iojs\\iojs.exe\"\n", argv[0]);
-        return 1;
-    }
+  // Validate the parameters
+  if (argc != 2) {
+    printf("usage: %s <iojsexe>\n", argv[0]);
+    printf("patchiojs patches the iojs.exe/node.exe to run in a Windows container.\n");
+    printf("%s example usage\n", argv[0]);
+    printf("   %s \"C:\\Program Files\\iojs\\iojs.exe\"\n", argv[0]);
+    return 1;
+  }
 
-    FILE *f = fopen(argv[1], "rb");
-    fseek(f, 0, SEEK_END);
-    long fsize = ftell(f);
-    fseek(f, 0, SEEK_SET);
+  FILE *f = fopen(argv[1], "rb");
+  fseek(f, 0, SEEK_END);
+  long fsize = ftell(f);
+  fseek(f, 0, SEEK_SET);
 
-    char *string = malloc(fsize + 1);
-    fread(string, fsize, 1, f);
-    fclose(f);
+  char *string = malloc(fsize + 1);
+  fread(string, fsize, 1, f);
+  fclose(f);
 
-    string[fsize] = 0;
+  string[fsize] = 0;
 
-    char *match = strstr('dnsopts.hints = dns.ADDRCONFIG;');
-    if (match) {
-      strncpy(match, 'dnsopts.hints = 0;//', 20);
-    }
-    f = fopen(argv[1], "wb");
-    fwrite (string , sizeof(char), fsize, f);
-    fclose (f);
+  char *match = strstr(string, "dnsopts.hints = dns.ADDRCONFIG;");
+  if (match) {
+    strncpy(match, "dnsopts.hints = 0;//", 20);
+  }
+  f = fopen(argv[1], "wb");
+  fwrite (string , sizeof(char), fsize, f);
+  fclose (f);
 
-    return 0;
+  return 0;
 }
